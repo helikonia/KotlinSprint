47ddbcb19e21e8877efa14a0de8fd607e47cbbc9
helikonia <helikonia99@gmail.com>
2025-06-12 16:01:10 +0200

    KS-1-1 online shop

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,2 +1,10 @@
 package org.example.lesson1
 
+fun main(){
+    val numberOfOrders: Int = 75
+    val welcomeMessage: String = "Thank you for placing your order!"
+    println(numberOfOrders)
+    println(welcomeMessage)
+
+
+}
