244c99f530ceb9df9361577b4720f8abcefd1fb8
helikonia <helikonia99@gmail.com>
2025-06-13 14:51:48 +0200

    KS-1-5 clock fixed

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -2,14 +2,17 @@ package org.example.lesson1
 
 fun main() {
 
-    val totalSecondsInSpace: Int = 6480
-    val hours: Int = totalSecondsInSpace / 3600
-    val remainingSeconds: Int = totalSecondsInSpace % 3600
-    val minutes: Int = remainingSeconds / 60
-    val seconds: Int = remainingSeconds % 60
+    val hours: Int = GAGARIN_SECONDS_IN_SPACE / SECONDS_IN_HOUR
+    val remainingSeconds: Int = GAGARIN_SECONDS_IN_SPACE % SECONDS_IN_HOUR
+    val minutes: Int = remainingSeconds / MINUTES_IN_HOUR
+    val seconds: Int = remainingSeconds % MINUTES_IN_HOUR
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
     println(formattedTime)
 
 }
+
+const val GAGARIN_SECONDS_IN_SPACE: Int = 6480
+const val SECONDS_IN_HOUR: Int = 3600
+const val MINUTES_IN_HOUR: Int = 60
