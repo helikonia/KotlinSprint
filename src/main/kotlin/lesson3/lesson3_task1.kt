5c4b8bbc8f075c17766bf2166ceee41aa925a476
helikonia <helikonia99@gmail.com>
2025-06-13 18:25:36 +0200

    KS-3-3 greeting

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson3
+
+fun main() {
+
+    var timeOfTheDay = "afternoon"
+    val userName = "User123"
+
+    println("Good $timeOfTheDay, $userName!")
+
+    timeOfTheDay = "evening"
+
+    println("Good $timeOfTheDay, $userName!")
+
+
+}
