515ffffa77c2e721f16679b58c288d68d6e5d6e5
helikonia <helikonia99@gmail.com>
2025-06-14 09:48:07 +0200

    KS-1-5 fixed const

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -2,8 +2,9 @@ package org.example.lesson1
 
 fun main() {
 
-    val hours: Int = GAGARIN_SECONDS_IN_SPACE / SECONDS_IN_HOUR
-    val remainingSeconds: Int = GAGARIN_SECONDS_IN_SPACE % SECONDS_IN_HOUR
+    val secondsInSpace = 6480
+    val hours: Int = secondsInSpace / SECONDS_IN_HOUR
+    val remainingSeconds: Int = secondsInSpace % SECONDS_IN_HOUR
     val minutes: Int = remainingSeconds / MINUTES_IN_HOUR
     val seconds: Int = remainingSeconds % MINUTES_IN_HOUR
 
@@ -13,6 +14,5 @@ fun main() {
 
 }
 
-const val GAGARIN_SECONDS_IN_SPACE: Int = 6480
 const val SECONDS_IN_HOUR: Int = 3600
 const val MINUTES_IN_HOUR: Int = 60
