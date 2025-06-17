26d3c709d884d480850c9a020b21b14e2e0762c6
helikonia <helikonia99@gmail.com>
2025-06-17 13:41:04 +0200

    KS-5-2 limited content

---
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson5
+
+fun main() {
+    println("Enter your date of birth")
+    val userBirthDate = readln(). toInt()
+    val currentYear = 2025
+    val userAge = currentYear - userBirthDate
+
+        val resultText = if (userAge >= AGE_OF_MAJORITY) {
+            "Show the screen with special content"}
+        else if (userAge == 16 || userAge == 17)
+        { "Show limited content"}
+        else
+        {"Back to main screen"}
+        println(resultText)
+    }
+const val AGE_OF_MAJORITY = 18
