ad2a5a9f244012c0e9e40e4bcb6c0b2624c5e3f3
helikonia <helikonia99@gmail.com>
2025-06-17 11:40:14 +0200

    KS-4-2 lines fixed

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,17 +1,23 @@
 package org.example.lesson4
 
 fun main() {
-    val minWeightAverage = 35
-    val maxWeightAverage = 100
-    val maxVolumeAverage = 100
-
     val weight1 = 20
     val volume1 = 80
     val weight2 = 50
     val volume2 = 100
 
-    println("Cargo weighting $weight1 kg and having a volume of $volume1 l matches the \"Average\" category: ${weight1 > minWeightAverage && weight1 <= maxWeightAverage && volume1 < maxVolumeAverage}")
-    println("Cargo weighting $weight2 kg and having a volume of $volume2 l matches the \"Average\" category: ${weight2 > minWeightAverage && weight2 <= maxWeightAverage && volume2 < maxVolumeAverage}")
+    println(
+        "Cargo weighting $weight1 kg and having a volume of $volume1 l matches the \"Average\" category:" +
+                " ${weight1 > MIN_WEIGHT_AVERAGE && weight1 <= MAX_WEIGHT_AVERAGE && volume1 < MAX_VOLUME_AVERAGE}"
+    )
+    println(
+        "Cargo weighting $weight2 kg and having a volume of $volume2 l matches the \"Average\" category:" +
+                " ${weight2 > MIN_WEIGHT_AVERAGE && weight2 <= MAX_WEIGHT_AVERAGE && volume2 < MAX_VOLUME_AVERAGE}"
+    )
 
 
 }
+
+const val MIN_WEIGHT_AVERAGE = 35
+const val MAX_WEIGHT_AVERAGE = 100
+const val MAX_VOLUME_AVERAGE = 100
