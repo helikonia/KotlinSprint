d712d87bde57975991debf2b80ce047e47aeeb3d
helikonia <helikonia99@gmail.com>
2025-06-24 21:57:02 +0200

    KS-9-4 project

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -5,8 +5,11 @@ fun main() {
     val mutableListNames = mutableListOf<String>()
 
     print("How many names do you want to enter? ")
-    val inputAmount = readln().toInt()
-    for (i in ZERO until inputAmount) {
+    val inputAmount = readln().toIntOrNull() ?: run {
+        println("Invalid number")
+        return
+    }
+    for (i in 0 until inputAmount) {
         print("Enter the name ")
         val name = readln().trim()
         mutableListNames.add(name)
@@ -15,8 +18,6 @@ fun main() {
     val mutableListLowerNames = mutableListNames.map { it.lowercase() }
     val filtered = mutableListLowerNames.filter { it.first() in vowels }
 
-    println("Names starting with vowel: ${filtered.joinToString(" ,")}")
+    println("Names starting with vowel: ${filtered.joinToString(", ")}")
     println("Overall: ${filtered.size}")
 }
-
-const val ZERO = 0
